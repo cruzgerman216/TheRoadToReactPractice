16c1e4504e4b096548189ae77dff85b2e6cf6dce
= <cruzgerman216@yahoo.com>
2019-04-05 20:34:22 -0700

    modified:   public/index.html 	modified:   src/App.js 	modified:   src/index.js

--- trtr/src/App.js
+++ trtr/src/App.js
@@ -1,22 +1,49 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
     //all declarations go outside of the return
-    var helloworld = "welcome to the jungle!";
-    var fullname = {
+    let helloworld = "welcome to the jungle!";
+    const fullname = {
       fname: "German",
       lname: "Cruz"
     };
-    const test = <h1>hey</h1>;
+    const list = [
+      {
+        title: "React",
+        url: "https://reactjs.org/",
+        author: "Jordan Walke",
+        num_comments: 3,
+        points: 4,
+        objectID: 0
+      },
+      {
+        title: "Redux",
+        url: "https://redux.js.org/",
+        author: "Dan Abramov, Andrew Clark",
+        num_comments: 2,
+        points: 5,
+        objectID: 1
+      }
+    ];
+
+    const todos = [
+      {
+        title: "Today I'm going to talk to stevethecameraman",
+        ID: 0
+      },
+      {
+        title: "Going to code in react today",
+        ID: 1
+      }
+    ];
+    const todoItems = todos.map((todo, index) => (
+      // Only do this if items have no stable IDs
+      <li key={todos.ID}>{todo.text}</li>
+    ));
     //jsx
-    return (
-      <div className="App">
-        <h1>tes</h1>
-      </div>
-    );
+    return <div className="App">{todoItems.title}</div>;
   }
 }
 
--- trtr/src/index.js
+++ trtr/src/index.js
@@ -2,6 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map(number => (
+    <li key={number.toString()}>{number}</li>
+  ));
+  return <ul>{listItems}</ul>;
+}
+
+const numbers = [1, 2, 3, 4, 5];
+ReactDOM.render(
+  <NumberList numbers={numbers} />,
+  document.getElementById("test")
+);
