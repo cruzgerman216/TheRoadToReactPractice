f8a9a5ebc664adc86d0020b4454b3d2e4a594e1b
= <cruzgerman216@yahoo.com>
2019-04-10 20:23:09 -0700

    modified:   trtr/public/index.html 	new file:   trtr/src/Animal.js 	modified:   trtr/src/index.js

---
+++ trtr/src/Animal.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Animal extends Component {
+  render() {
+    const test = <h1>Inside animals.js</h1>;
+
+    return <div>{test}</div>;
+  }
+}
+
+export default Animal;
