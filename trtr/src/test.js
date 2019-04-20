ba763d5929d8ceb3a83b25ffd3d74f6e13acae34
= <cruzgerman216@yahoo.com>
2019-04-19 22:30:34 -0700

    modified:   trtr/public/index.html 	modified:   trtr/src/index.js 	modified:   trtr/src/test.js

--- trtr/src/test.js
+++ trtr/src/test.js
@@ -1,14 +1,28 @@
 import React, { Component } from "react";
 
 class Test extends Component {
-  getGreeting() {
-    return "welcome to react";
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
   }
 
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
   render() {
     return (
       <div>
-        <h1>{this.getGreeting()}</h1>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
       </div>
     );
   }
